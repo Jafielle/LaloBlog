36e6163a52f959a9c645436afaa8825cd4b17952
Jafielle <abrilgomez.aray@gmail.com>
2023-03-03 21:31:27 -0500

    filter active button add class

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-//Filter js. Do I need it
-$(document).readyState(function () {
-      $('.filter-item').click(function () {
-            const value = $(this).attr('data-filter')
-            if (value == 'all') {
-                  $('.post-box').show('1000')
+//alert("hello") //conected
+
+//Filter js. Debug
+$(document).ready(function () {
+      $(".filter-item").click(function () {
+            const value = $(this).attr("data-filter");
+            if (value == "all") {
+                  $(".post-box").show("1000");
             }
             else {
-                  $('.post.box').not('.' + value).hide('1000')
-                  $('.post.box').filter('.' + value).show('1000')
+                  $(".post.box").not("." + value).hide("1000");
+                  $(".post.box").filter("." + value).show("1000");
             }
-      })
-})
+      });
+
+      //Active button. Works
+      $(".filter-item").click(function () {
+            $(this).addClass("active-filter").siblings().removeClass("active-filter");
+      });
+});
+
+
+
