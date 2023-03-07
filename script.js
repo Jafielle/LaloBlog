9a11277eca0c305e11087784ee46ce2c4cc285a0
Jafielle <abrilgomez.aray@gmail.com>
2023-03-06 23:59:42 -0500

    Adding filter JS

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+//alert('Conected')
+
+filterObjects("all")
+
+function filterObjects(c){
+      let i;
+      let x = document.getElementsByClassName("post-box");
+      if(c == "all") c == "";
+      for (i = 0; i < x.length; i++){
+            removeClass(x[i], "show");
+            if(x[i].className.indexOf(c) > -1)addClass(x[i], "show")
+      }
+}
+
+function addClass(element, name) {
+      let i;
+      let arr1 = element.className.split(" ");
+      let arr2 = name.split(" ");
+      for (i = 0; i < arr2.length; i++) {
+            if (arr1.indexOf(arr2[i]) == -1) {
+                  element.className += " " + arr2[i]
+            }
+      }
+
+}
 
 
+function removeClass(element, name) {
+      let i;
+      let arr1 = element.className.split(" ");
+      let arr2 = name.split(" ");
+      for (i = 0; i < arr2.length; i++) {
+            while (arr1.indexOf(arr2[i]) > -1) {
+                  arr1.splice(arr1.indexOf(arr2[i]), 1);
+            }
+      }
+      element.className = arr1.join(" ")
+}
 
