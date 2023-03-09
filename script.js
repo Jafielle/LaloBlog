1cd59b1ff743307a725e35df0ba50f00be551965
Jafielle <abrilgomez.aray@gmail.com>
2023-03-08 23:38:50 -0500

    Changing js logic

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //alert('Conected')
 
+/*
 //filterObjects("all")
 function filterObjects(filter) {
       let i;
@@ -43,3 +44,48 @@ function removeClass(element, name) {
 $(".filter-item").click(function () {
       $(this).addClass("active-filter").siblings().removeClass("active-filter");
 })
+
+*/
+
+const filterBtn = document.getElementsByClassName("filter-item");
+
+const postBox = document.getElementsByClassName("post-box");
+
+for (let i = 1; i < filterBtn.length; i++) {
+      btns[i].addEventListener('click', filterImg);
+}
+
+function setActiveBtn(e) {
+
+      btns.forEach(btn => {
+            btn.classList.remove('active-filter');
+      });
+
+      e.target.classList.add('active-filter');
+}
+
+
+function filterImg(e) {
+      setActiveBtn(e);
+
+      postBox.forEach(post => {
+            post.classList.remove('img-shrink');
+            post.classList.add('img-expand');
+
+            //Get data from data attributes
+            //Get image type data
+            const imgType = parseInt(img.dataset.img);
+            //Get button type data
+            const btnType = parseInt(e.target.dataset.btn);
+
+            /*
+            If the image type and the type of the clicked button are NOT the same
+            */
+
+            if (imgType !== btnType) {
+                  //Hide the image
+                  post.classList.remove('expand');
+                  post.classList.add('shrink');
+            }
+      })
+}
