9807878e4a3efb586abc811da361901186a004de
Jafielle <abrilgomez.aray@gmail.com>
2023-04-07 23:55:30 -0500

    Adding share btns js

---
+++ shareScript.js
@@ -0,0 +1,30 @@
+/*
+https://api.whatsapp.com/send?text=[post-title] [post-url]
+
+https://www.facebook.com/sharer.php?u=[post-url]
+
+https://twitter.com/share?url=[post-url]&text=[post-title]&via=[via]&hashtags=[hashtags]
+
+https://plus.google.com/share?url=[post-url]
+
+
+https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]
+
+
+*/
+
+alert("Conected")
+
+const fbkShareBtn = document.querySelector('.facebook-share-btn');
+const twitterShareBtn = document.querySelector('.facebook-share-btn');
+const linkedinShareBtn = document.querySelector('.linkedin-share-btn');
+const gmailShareBtn = document.querySelector('.gmail-share-btn');
+
+const whatsappShareBtn = document.querySelector('.whatsapp-share-btn');
+
+function init() {
+      console.log('hello');
+}
+
+
+
