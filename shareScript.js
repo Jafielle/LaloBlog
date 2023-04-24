0f69de04fbd8ff8c7f9244114824deff89bf9a08
Jafielle <abrilgomez.aray@gmail.com>
2023-04-24 01:25:05 -0500

    Added working share post btns

--- shareScript.js
+++ shareScript.js
@@ -1,53 +1,40 @@
 /*
-https://api.whatsapp.com/send?text=[post-title] [post-url]
+const shareBtn = document.getElementById('shareBtn')
 
-      https://www.facebook.com/sharer.php?u=[post-url]
+shareBtn.addEventListener('click', event => {
 
-      https://twitter.com/share?url=[post-url]&text=[post-title]&via=[via]&hashtags=[hashtags]
-
-https://plus.google.com/share?url=[post-url]
-
-
-      https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]
-
-      $email = 'mailto:?subject=' . $[post-title] . '&body=Check out this site: '. $[post-url] .'" title="Share by Email';
-
-
-https://www.youtube.com/watch?v=OfLvQ8KtW2g
+      // Check for Web Share api support
+      if (navigator.share) {
+            // Browser supports native share api
+            navigator.share({
+                  text: 'Please read this great article: ',
+                  url: 'https://www.google.com/'
+            }).then(() => {
+                  console.log('Thanks for sharing!');
+            })
+                  .catch((err) => console.error(err));
+      } else {
+            // Fallback
+            alert("The current browser does not support the share function. Please, manually share the link")
+      }
+});
 */
+//post link
+const link = encodeURI('https://google.com');
+//sending msg
+const msg = encodeURIComponent('Encontré este artículo interesante!');
+const title = encodeURIComponent('Article or Post Title Here');
+const fb = document.querySelector('.facebook');
+fb.href = `https://www.facebook.com/share.php?u=${link}`;
+const twitter = document.querySelector('.twitter');
+//mind hashtags
+twitter.href = `http://twitter.com/share?&url=${link}&text=${msg}&hashtags=alimentos,bioquímica,ciencia`;
+const linkedIn = document.querySelector('.linkedin');
+linkedIn.href = `https://www.linkedin.com/sharing/share-offsite/?url=${link}`;
+const reddit = document.querySelector('.reddit');
+reddit.href = `http://www.reddit.com/submit?url=${link}&title=${title}`;
+const whatsapp = document.querySelector('.whatsapp');
+whatsapp.href = `https://api.whatsapp.com/send?text=${msg}: ${link}`;
+const telegram = document.querySelector('.telegram');
+telegram.href = `https://telegram.me/share/url?url=${link}&text=${msg}`;
 
-const fbkShareBtn = document.querySelector('.facebook-share-btn');
-const twitterShareBtn = document.querySelector('.facebook-share-btn');
-const linkedinShareBtn = document.querySelector('.linkedin-share-btn');
-const gmailShareBtn = document.querySelector('.gmail-share-btn');
-
-const whatsappShareBtn = document.querySelector('.whatsapp-share-btn');
-
-function init() {
-      const headerImg = document.querySelector('.header-img');
-
-
-      let postUrl = encodeURI(document.location.href);
-      let postShareTitle  = encodeURI('Hola, quiero compartir este articulo');
-      let postImg = encodeURI(headerImg.src)
-
-      //fbkShareBtn.setAttribute("href", `https://www.facebook.com/sharer.php?u=${postUrl}`);
-
-      fbkShareBtn.setAttribute("href", `https://www.facebook.com/sharer.php?u=[postUrl]`);
-
-      
-
-      twitterShareBtn.setAttribute("href", `https://twitter.com/share?url=${post-url}&text=${postShareTitle}`);
-
-      linkedinShareBtn.setAttribute("href", `https://www.linkedin.com/shareArticle?url=${post-url}&text=${postShareTitle}`);
-
-      
-      whatsappShareBtn.setAttribute("href", `hhttps://api.whatsapp.com/send?text=${postShareTitle} ${post-url}`);
-      
-      /*linkedinShareBtn.setAttribute("href", ` $email = 'mailto:?subject=' . $[post-title] . '&body=Check out this site: '. $[post-url] .'" title="Share by Email';);
-      */
-
-
-}
-
-init();
